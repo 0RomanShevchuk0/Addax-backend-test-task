9aa5bb03b0933183887cd505efcdb4adb0eaaec1
RomanShevchuk0 <roman.shevchuk@dmorpheus.design>
2025-02-15 22:28:30 +0100

    docker init

--- src/config/env.ts
+++ src/config/env.ts
@@ -3,8 +3,9 @@ import dotenv from "dotenv"
 dotenv.config()
 
 export const env = {
-  PORT: process.env.PORT || 4200,
+  PORT: process.env.PORT,
   MONGO_URI: process.env.MONGO_URI,
   DB_NAME: process.env.DB_NAME,
-  JWT_SECRET: process.env.JWT_SECRET || 'default-secret',
+  JWT_SECRET: process.env.JWT_SECRET,
+  DATABASE_URL: process.env.DATABASE_URL,
 }
